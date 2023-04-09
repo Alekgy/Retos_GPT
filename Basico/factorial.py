f63fb9ac841388a5ea57d8c65193bb67864b3dbf
alejandro <alekgy@gmail.com>
2023-04-08 23:22:35 -0500

    nuevos retos por chatGPT

--- Basico/factorial.py
+++ Basico/factorial.py
@@ -1,5 +1,18 @@
+"""
+    DESCRIPCION DEL RETO:
 
-def factorial(user_number):
+    Escribe un programa en Python que le solicite al usuario que ingrese un número entero
+    positivo y luego calcule e imprima el factorial de ese número.
+
+    Recuerda que el factorial de un número entero positivo n es el producto de todos Los
+    números enteros positivos desde 1 hasta n.
+
+    Por ejemplo, el factorial de 5 se calcula como 1 x 2 x 3 x 4 x 5 = 120.
+
+"""
+
+
+def factorial(user_number): 
     if user_number == 0 or user_number == 1:
         result = 1
     elif user_number > 1:
