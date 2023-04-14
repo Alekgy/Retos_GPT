dab8aab533a1e6e0acd97e51b0df11f0b891742b
alejandro <alekgy@gmail.com>
2023-04-14 13:02:20 -0500

    add new challenges to chatGPT

--- Intermedio/counting_words.py
+++
@@ -1,7 +0,0 @@
-"""
-    DESCRIPCION DEL RETO:
-
-    Escribe un programa en Python que lea un archivo de texto y determine la frecuencia de cada palabra en el archivo. Luego, tu programa debe imprimir las 10 palabras más comunes en orden descendente de frecuencia.
-
-    Tu programa debe ignorar los caracteres de puntuación y no debe distinguir entre mayúsculas y minúsculas.
-"""
