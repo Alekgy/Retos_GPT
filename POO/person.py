bc5cfbc9d190d873966eecbf17b9606f71f05bd6
alejandro <alekgy@gmail.com>
2023-04-26 23:41:30 -0500

    inicio de retos GPT con POO de objetivo y adicion de primero retos

---
+++ POO/person.py
@@ -0,0 +1,19 @@
+"""
+    DESCRIPCION DEL RETO:
+
+    Crea una clase llamada "Persona" que tenga los atributos "nombre" y "edad". La clase debe tener un método llamado "presentarse" que imprima un mensaje de la forma "Hola, mi nombre es {nombre} y tengo {edad} años".
+"""
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    
+    def presentation(self):
+        print(f'Hola, mi nombre es {self.name} mi edad es {self.age}')
+
+saludo = Person('Alejandro', 28)
+
+saludo.presentation()
