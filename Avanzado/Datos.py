df5ef63c8d639fc82b251636c7d3ba6b811b7365
Alejandro <Alekgy@gmail.com>
2023-05-03 13:37:09 -0500

    a

---
+++ Avanzado/Datos.py
@@ -0,0 +1,25 @@
+"""
+    DESCRIPCION DEL RETO:
+
+    Escribe un programa en Python que lea un archivo CSV que contenga datos sobre una lista de productos. Cada fila del archivo debe contener información sobre un solo producto, con campos para el nombre del producto, la categoría, el precio y la cantidad disponible.
+
+    - El programa debe ser capaz de realizar las siguientes tareas:
+
+    - Leer los datos del archivo CSV y almacenarlos en una estructura de datos adecuada.
+
+    - Permitir al usuario buscar productos por nombre o por categoría y mostrar los resultados.
+
+    - Permitir al usuario agregar nuevos productos a la lista.
+
+    - Permitir al usuario modificar el precio o la cantidad disponible de un producto existente.
+
+    - Permitir al usuario guardar los cambios en el archivo CSV.
+
+    Para hacerlo más interesante, puedes implementar algunas de las siguientes funcionalidades adicionales:
+
+    - Validar que los campos ingresados por el usuario sean válidos (por ejemplo, que el precio sea un número válido y la cantidad disponible sea un número entero positivo).
+    - Ordenar la lista de productos por nombre, precio o cantidad disponible.
+    - Permitir al usuario eliminar un producto de la lista.
+    - Agregar una opción para mostrar solo productos con una cantidad disponible menor que un valor dado.
+    - Agregar una opción para mostrar solo productos que estén dentro de un rango de precios dado.
+"""
