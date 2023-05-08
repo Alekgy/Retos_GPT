35fae904e5c20bc2286f68eef5920fcbedef87e2
alejandro <alekgy@gmail.com>
2023-05-08 12:37:33 -0500

    adicion nuevo reto

--- Avanzado/Datos.py
+++ Avanzado/Datos.py
@@ -23,3 +23,83 @@
     - Agregar una opción para mostrar solo productos con una cantidad disponible menor que un valor dado.
     - Agregar una opción para mostrar solo productos que estén dentro de un rango de precios dado.
 """
+import sqlite3
+import csv
+
+ruta_csv = '/home/alejandro/ALEJANDRO/Retos_GPT/Avanzado/Archivo.csv'
+ruta_db = '/home/alejandro/ALEJANDRO/Retos_GPT/Avanzado/DB.db'
+
+class Productos:
+    def __init__(self, nombre, categoria, ruta_db):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.productos = []
+        self.read(ruta_db)
+
+
+    def read(self, ruta_db):
+        with open(ruta_db, 'r') as archivo:
+            lector_csv = csv.reader(archivo)
+
+            for fila in lector_csv:
+                nombre, categoria, precio, cantidad = fila
+                self.productos.append({
+                    'nombre': nombre,
+                    'categoria': categoria,
+                    'precio': float(precio),
+                    'cantidad': int(cantidad)
+                })
+
+    def search(self):
+        resultados = []
+        for producto in self.productos:
+            if self.nombre.lower() in producto['nombre'].lower():
+                resultados.append(producto)
+            
+            elif self.categoria.lower() == producto['categoria'].lower():
+                resultados.append(producto)
+
+        return resultados
+
+    def add(self, nombre, categoria, precio, cantidad):
+
+        self.productos.append({
+            'nombre': nombre,
+            'categoria': categoria,
+            'precio': float(precio),
+            'cantidad': int(cantidad)
+        })
+
+    def modify(self, precio=None, cantidad=None):
+
+        for producto in self.productos:
+            if self.nombre.lower() == producto['nombre'].lower():
+                if precio is not None:
+                    producto['precio'] = float(precio)
+                if cantidad is not None:
+                    producto['cantidad'] = int(cantidad)
+                return True
+        return False
+
+
+def conexion(ruta_csv, ruta_db):
+    conexion = sqlite3.connect(ruta_db)
+
+    cursor = conexion.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Productos
+                    (id INTEGER PRIMARY KEY,
+                    Nombre TEXT,
+                    Categoria TEXT,
+                    Precio INTEGER,
+                    Cantidad_disponible INTEGER)''')
+
+    with open(ruta_csv, newline='') as csvfile:
+        content = csv.DictReader(csvfile)
+        for fila in content:
+            cursor.execute('INSERT INTO Productos (Nombre, Categoria, Precio, Cantidad_disponible) VALUES (?, ?, ?, ?)', (fila['Nombre'], fila['Categoria'], fila['Precio'], fila['Cantidad_disponible']))
+
+    conexion.commit()
+    conexion.close()
+
+def save_change():
+    pass
